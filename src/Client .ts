359d2d30bea4bdf00d4ac140f59ba8fa3781d34f
Paul Johnson <93623067+PaulJohnson1@users.noreply.github.com>
2021-11-03 00:11:29 +0000

    make client extend player

--- src/Client .ts
+++ src/Client .ts
@@ -1,12 +1,12 @@
 import * as WebSocket from "ws";
-import Game from "./Game";
+import Player from "./Entity/Player";
 
-export default class Client {
-  public game: Game;
+export default class Client extends Player {
   public socket: WebSocket;
 
   constructor(game: Game, socket: WebSocket) {
-    this.game = game;
+    super(game);
+
     this.socket = socket;
   }
 }
