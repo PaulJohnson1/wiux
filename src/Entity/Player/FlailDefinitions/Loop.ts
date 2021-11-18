e0f80df0183c2486d2f3dc4756f1aa7fa6dd60f2
Paul Johnson <93623067+PaulJohnson1@users.noreply.github.com>
2021-11-18 20:34:47 +0000

    add loop weapon

---
+++ src/Entity/Player/FlailDefinitions/Loop.ts
@@ -0,0 +1,44 @@
+import BaseDefinition from "./BaseDefinition";
+import Rope from "../Rope/Rope";
+import Flail from "../Flail";
+import Player from "../Player";
+
+export default class BasicFlail extends BaseDefinition {
+
+  constructor(owner: Player) {
+    super(owner);
+
+    this.flails[0] = new Flail(this.owner.game, this.owner);
+    this.ropes[0] = new Rope(
+      this.owner.game,
+      this.owner,
+      this.flails[0],
+      3,
+      0.1,
+      20
+    );
+
+    const length = 3;
+
+    for (let i = 0; i < length; i++) {
+      this.flails[i + 1] = new Flail(this.owner.game, this.owner);
+      this.ropes[i + 1] = new Rope(
+        this.owner.game,
+        this.flails[i],
+        this.flails[i + 1],
+        3,
+        0.1,
+        20
+      );
+    }
+
+    this.ropes[length + 1] = new Rope(
+      this.owner.game,
+      this.owner,
+      this.flails[length],
+      3,
+      0.1,
+      20
+    );
+  }
+}
