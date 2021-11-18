a774b6c2c7829ca0bdcf4e22382b8b2476a27e00
Paul Johnson <93623067+PaulJohnson1@users.noreply.github.com>
2021-11-18 13:50:17 +0000

    change building and running

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import * as WebSocket from "ws";
 import Server from "./Server";
 
-export default function () {
-  const wss = new WebSocket.Server({ port: 1234 });
+const wss = new WebSocket.Server({ port: 1234 });
 
-  const server = new Server(wss);
+const server = new Server(wss);
 
-  console.log("running");
-}
+console.log("running");
