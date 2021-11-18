bb2c19d5a633b9f7af0622833620178ce2c149a4
Paul Johnson <93623067+PaulJohnson1@users.noreply.github.com>
2021-11-18 19:48:46 +0000

    add new weapon config

---
+++ src/Entity/Player/FlailDefinitions/DoublePendulum.ts
@@ -0,0 +1,31 @@
+import BaseDefinition from "./BaseDefinition";
+import Rope from "../Rope/Rope";
+import Flail from "../Flail";
+import Player from "../Player";
+
+export default class BasicFlail extends BaseDefinition {
+
+  constructor(owner: Player) {
+    super(owner);
+
+    this.flails[0] = new Flail(this.owner.game, this.owner);
+    this.ropes[0] = new Rope(
+      this.owner.game,
+      this.owner,
+      this.flails[0],
+      3,
+      0.1,
+      20
+    );
+
+    this.flails[1] = new Flail(this.owner.game, this.owner);
+    this.ropes[1] = new Rope(
+      this.owner.game,
+      this.flails[0],
+      this.flails[1],
+      3,
+      0.1,
+      20
+    );
+  }
+}
