e3872a60309c7cf955b8909a0116945385badfa2
Paul Johnson <93623067+PaulJohnson1@users.noreply.github.com>
2021-12-21 20:31:46 +0000

    make food not as bad

--- src/Entity/Food/Food.ts
+++ src/Entity/Food/Food.ts
@@ -4,39 +4,29 @@ import Game from "../../Game";
 import { Writer } from "../../Coder";
 
 export default class Food extends BaseEntity {
-  constructor(game: Game, area: number) {
+  public score: number;
+
+  constructor(game: Game, size: number, score: number) {
     super(game);
 
-    this.area = area;
+    this.size = size;
+    this.score = score;
 
-    this.knockback = 0.1;
+    this.knockback = 0.05;
+    this.resistance = 2
 
     this.collides = true;
     this.detectsCollision = true;
     this.isAffectedByWind = true;
-    this.onMinimap = true;
+    this.onMinimap = false;
     
     this.color = Math.random() * 360;
   }
 
   onCollisionCallback(entity: BaseEntity) {
-    if (entity === this) return;
-
-    if (entity instanceof Food) {
-      if (this.size + entity.size > 200) return;
-
-      if (this.size > entity.size) {
-        entity.terminate();
-        this.area += entity.area * 0.7;
-      } else {
-        this.terminate();
-        entity.area += this.area * 0.7;
-      }
-    }
-
     if (!(entity instanceof Flail)) return;
 
-    entity.area += this.area;
+    entity.area += this.score;
     super.terminate();
   }
 }
